4b0848fec5e064719f2a883fcb1670fb77203ef6
Tyler M <tylermcbride@uchicago.edu>
2024-08-09 15:36:16 -0700

    Added line chart rendering to app

--- lake-powell-database/src/App.tsx
+++ lake-powell-database/src/App.tsx
@@ -1,12 +1,14 @@
 import Dropdown from './dropdown'
+import Chart from './chart'
 import './App.css'
 
 function App() {
-  return (
-    <div>
-        <Dropdown />
-    </div>
-  )
-}
+    return (
+        <>
+        <div><Dropdown/></div>
+        <div><Chart/></div>
+        </>
+    );
+};
 
-export default App
+export default App;
---
+++ lake-powell-database/src/chart.tsx
@@ -0,0 +1,51 @@
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+  
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+Legend
+);
+
+const Chart: React.FC = () => {
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+            label: 'My First dataset',
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            },
+        ],
+    };
+
+    const options = {
+        scales: {
+            y: {
+              beginAtZero: true
+            }
+        }
+    };
+
+    return (
+        <div style={{ width: '600px', height: '400px' }}>
+            <Line data={data} options={options}/>
+        </div>
+        );
+};
+
+export default Chart;
